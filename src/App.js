fa183d1cb59c813a83bc5a75e3d2fca835bdfccf
Juanca <clavijojc@gmail.com>
2018-11-21 22:22:24 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import { AppBar, Typography } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import ForeCasteExtendedContainer from './containers/ForeCasteExtendedContainer';
+import LocationListContainer from './containers/LocationListContainer'
+
+/* import logo from './logo.svg';*/
 import './App.css';
+const cities = [
+  "Buenos Aires,ar"
+  , "Washington,us"
+  , "Caracas,ve"
+  , "Toronto,ca"
+  , "Quito,ec"];
+
+/* pide que le pasememos como parametros un "Reducer" que es lo que me dice que sucedio no como.*/
+
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grid fluid>
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                WEATHER APP
+    </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col sm={12} xl={6}>
+            <LocationListContainer cities={cities}></LocationListContainer>
+          </Col>
+          <Col sm={12} xl={6}>
+            <Paper elevation={4}>
+              <div className="details">
+                {/* si tengo un valor en city renderizo el forecastextended sino, no */
+                  <h1>
+                    <ForeCasteExtendedContainer></ForeCasteExtendedContainer>
+                  </h1>
+                }
+              </div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
